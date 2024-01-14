aafd4df6f42db4fb727e2de7b507ceef2d87bc01
CW-RanjitMahadik <ranjit@carwale.com>
2024-01-14 15:43:17 +0530

    - added new column created_at

--- db/schemas/schema.sql
+++ db/schemas/schema.sql
@@ -1,4 +1,5 @@
 CREATE TABLE url_shortener(
     short_url varchar(10) primary key,
-    full_url varchar(265)
+    full_url varchar(265),
+    created_at bigint default extract(epoch from now())
 );
